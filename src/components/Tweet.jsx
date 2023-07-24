334d602ae71e4285e94d6382334a659a0e6de5c0
Sophia Ziesch <s.ziesch@posteo.de>
2023-07-24 17:44:55 +0200

    lab solved

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,39 +1,25 @@
-function Tweet() {
-  return (
-    <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+function Tweet({ tweet }) {
+	return (
+		<div className="tweet">
+			<ProfileImage image={tweet.user.image} />
+			<div className="body">
+				<div className="top">
+					<User name={tweet.user.name} handle={tweet.user.handle} />
+					<Timestamp time={tweet.timestamp} />
+				</div>
+				<Message message={tweet.message} />
+				<Actions />
+			</div>
 
-          <span className="timestamp">Nov 30, 2020</span>
-        </div>
-
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
-      </div>
-
-      <i className="fas fa-ellipsis-h"></i>
-    </div>
-  );
+			<i className="fas fa-ellipsis-h"></i>
+		</div>
+	);
 }
 
 export default Tweet;
